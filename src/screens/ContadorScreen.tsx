3cbbb2cd0cb11ca4c47008c7a87f9ce5e5a05507
astabaresg <44148319+astabaresg@users.noreply.github.com>
2022-09-20 01:12:55 -0500

    Commit Inicial

---
+++ src/screens/ContadorScreen.tsx
@@ -0,0 +1,59 @@
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import { Fab } from '../components/Fab';
+
+export const ContadorScreen = () => {
+  const [contador, setContador] = useState(10);
+
+  return (
+    <View
+      style={styles.container}>
+      <Text
+        style={styles.title}>
+        contador: {contador}
+      </Text>
+
+      <Fab
+        title='+1'
+        position='br'
+        onPress={ () => setContador(contador + 1) }
+      />
+
+      <Fab
+        title='-1'
+        position='bl'
+        onPress={ () => setContador(contador - 1) }
+      />
+
+      {/* <TouchableOpacity 
+        onPress={() => setContador(contador + 1)}
+        style={styles.fabLocationBR}
+      >
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>+1</Text>
+        </View>
+      </TouchableOpacity>
+      <TouchableOpacity 
+        onPress={() => setContador(contador - 1)}
+        style={styles.fabLocationBL}
+      >
+        <View style={styles.fab}>
+          <Text style={styles.fabText}>-1</Text>
+        </View>
+      </TouchableOpacity> */}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+    },
+
+    title:{
+        textAlign: 'center',
+        fontSize: 40,
+        top: -15,
+    },
+});
